92dc2e86fc46f5c7f97703c8f440f7b04abe228f
KreakxX <Henrik.standke@web.de>
2025-07-06 22:46:08 +0200

    Training done, and early stopping implemented

--- Classification/Imageclassification.py
+++ Classification/Imageclassification.py
@@ -1,6 +1,9 @@
 import keras 
 from keras import layers,models
 import tensorflow as tf
+import os
+from PIL import Image
+import tensorflow_datasets as tfds
 
 model = keras.Sequential(
   [layers.Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),   # 32 filters like finding shapes and corners that,  3,3 is for the size if smaller = feiner
@@ -15,9 +18,32 @@ model = keras.Sequential(
 
 model.compile(optimizer="adam",loss="sparse_categorical_crossentropy", metrics=["accuracy"] ) # compile
 
-# load Dataset
+# load Dataset  # data augmentation but not here because dataset is ideal
 
+(train_ds, test_ds), info = tfds.load(
+    'cats_vs_dogs',
+    split=['train[:80%]', 'train[80%:]'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True
+)
+def preprocess(image, label):
+    image = tf.cast(image, tf.float32) / 255.0
+    image = tf.image.resize(image, [128, 128])
+    return image, label
 
+train_ds = train_ds.map(preprocess).batch(32)
+test_ds = test_ds.map(preprocess).batch(32)
 
-#model.fit(train_ds,epochs=10)
+early_stopping = keras.callbacks.EarlyStopping(
+    monitor='val_loss',      
+    patience=3,              
+    restore_best_weights=True  
+)
+model.fit(
+    train_ds,
+    validation_data=test_ds,
+    epochs=10,
+    callbacks=early_stopping
+)
 model.save("Cat_Dog_Classifier.keras")
