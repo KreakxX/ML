239f9bef21403673f3781f82de6a83819c998b7c
KreakxX <Henrik.standke@web.de>
2025-07-12 13:14:47 +0200

    Food_Classifier.keras

--- Classification/FoodClassifier.py
+++ Classification/FoodClassifier.py
@@ -10,7 +10,12 @@ from datasets import load_dataset
 
 # dataset preparation 
 
-train_ds, test_ds,info = tfds.load('food101', split=['train', 'validation'], as_supervised=True,with_info=True)
+(train_ds, test_ds), info = tfds.load(
+    'food101', 
+    split=['train[:40%]', 'validation[:40%]'], 
+    as_supervised=True,
+    with_info=True
+)
 
 # for better learning
 def dataAugmentaion(image,label):
@@ -22,7 +27,7 @@ def dataAugmentaion(image,label):
 # data preparation
 def preprocess(image, label):
     image = tf.cast(image, tf.float32) / 255.0  
-    image = tf.image.resize(image, [128, 128]) 
+    image = tf.image.resize(image, [256, 256]) 
     return image, label
 
 train_ds = train_ds.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE)
@@ -56,9 +61,27 @@ model.compile(optimizer="adam", loss="sparse_categorical_crossentropy",
 # training
 earlyStopping = keras.callbacks.EarlyStopping(
   monitor="val_loss",
-  patience=10,
+  patience=3,
   restore_best_weights=True
 )
 
-model.fit(train_ds, validation_data=test_ds,epochs=10,callbacks=[earlyStopping])
+model.fit(train_ds, validation_data=test_ds,epochs=15,callbacks=[earlyStopping])
 model.save("Food_classfier.keras")
+
+
+def testFoodClassififer(img_path):
+
+    foodModel = keras.models.load_models("Food_classifier.keras")
+
+    img = Image.open(img_path)
+    img_resized = img.resize((256,256))
+    img_arr = np.array(img_resized) / 255.0
+    img_arr = np.expand_dims(img_arr,axis=0)
+
+    pred = foodModel.predict(img_arr)[0]
+    prediction = class_names[pred]
+    print(prediction)
+
+
+
+# Summary more Training loss still too high because, too much classes for classification
